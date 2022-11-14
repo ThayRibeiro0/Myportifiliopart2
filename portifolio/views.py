087b2c3ef80c0c5af903dd606e4cade381e6c79e
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-11-13 21:51:25 -0800

    7 commit

--- portifolio/views.py
+++ portifolio/views.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template
+# import numpy as np
+# import matploatlib.pyplot as plt
+# from mpl_toolkits.basemap import Basemap
 
 views = Blueprint('views', __name__)
 
@@ -17,6 +20,30 @@ def about():
 @views.route('/experiences')
 def experiences():
     return render_template("experiences.html")
+
+# def map():
+#     austin = (-97.75, 30.25)
+#     hawaii = (-157.8, 21.3)
+#     washington = (-77.01, 38.90)
+#     chicago = (-87.68, 41.83)
+#     losangeles = (-118.25, 34.05)
+    
+#     m = Basemap(projection='merc', llcrnrlat=10,urcrnrlat=50,llcrnrlon=-160,urcrnrlon=-60)
+
+#     m.drawcoastlines()
+#     m.fillcontinents(color='lightgray', lake_color='lightblue')
+#     m.drawparallels(np.arange(-90.,91.,30.))
+#     m.drawmeridians(np.arange(-180.,181.,60.))
+#     m.drawmapboundary(fill_color='aqua')
+
+#     m.drawcountries()
+
+#     x,y = m(*zip(*[hawaii, austin, washington, chicago, losangeles]))
+
+#     m.plot(x,y, marker='o', markersize=6, markerfacecolor='red', linewidth=0)
+
+#     plt.title("Mercator Projection")
+#     plt.show()
 
 @views.route('/skills')
 def skills():
