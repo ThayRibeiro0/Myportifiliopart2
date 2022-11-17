c53f3dd3f5399f1bdd1dcc2e0420b0f740cc4bde
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-11-17 12:47:41 -0800

    11 commit

--- portifolio/static/index.js
+++ portifolio/static/index.js
@@ -20,18 +20,18 @@ window.addEventListener('hashchange', () => {
     }, 255)
 });
 
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     slidesPerGroup: 1,
-//     loop: true,
-//     loopFillGroupWithBlank: true,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
+var swiper = new Swiper(".move-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
