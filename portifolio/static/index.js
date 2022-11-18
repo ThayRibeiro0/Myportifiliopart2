4266d61e8ae9ec35bcb56e0b28ba86b53632da5c
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-11-18 00:17:44 -0800

    11 commit

--- portifolio/static/index.js
+++ portifolio/static/index.js
@@ -22,16 +22,30 @@ window.addEventListener('hashchange', () => {
 
 var swiper = new Swiper(".move-content", {
     slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    spaceBetween: 25,
     loop: true,
-    loopFillGroupWithBlank: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+      0:{
+        slidesPerView: 1,
+      },
+      520:{
+        slidesPerView: 2,
+      },
+      950:{
+        slidesPerView: 3,
+      },
     },
   });
